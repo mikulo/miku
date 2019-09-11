99ddc5184cb95a80c08b73145fac58114110e575
mikulo <46894621+mikulo@users.noreply.github.com>
2019-09-11 12:27:35 +0800

    Update miku.sh

--- miku.sh
+++ miku.sh
@@ -104,6 +104,7 @@ then
 	time echo "scale=$pi; a(1)*4" | bc -l
 elif	[ "$m1" == 7 ]
 then
+	rm -f/usr/loacl/sbin/miku
 	wget -N  https://raw.githubusercontent.com/mikulo/miku/master/miku.sh
 	chmod -R 0777 /root/miku.sh
 	mv -u /root/miku.sh /usr/local/sbin/miku
