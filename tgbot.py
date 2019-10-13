5f7c5ef21163405f5c32a9ef31f40fdba27cd41a
mikulo <46894621+mikulo@users.noreply.github.com>
2019-10-13 12:38:32 +0800

    Update tgbot.py

--- tgbot.py
+++ tgbot.py
@@ -1,9 +1,9 @@
 import telegram
 import sys
+bot = telegram.Bot(token='716376253:AAEKXSr17ZSSAuo-gTjWSo2IOFDCSVziZqs')
 if  len(sys.argv)==0:
     print('没有消息参数')
 elif  len(sys.argv)>1:
     print('消息参数过多')
 else:
-    bot = telegram.Bot(token='716376253:AAEKXSr17ZSSAuo-gTjWSo2IOFDCSVziZqs')
     bot.send_message(chat_id='-1001364247522', text=sys.argv[0])
