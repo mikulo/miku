3e256ef04c8467a634562b0f93a50dffc4cff2fe
mikulo <46894621+mikulo@users.noreply.github.com>
2019-10-21 17:42:03 +0800

    Update meizitu.py

--- meizitu.py
+++ meizitu.py
@@ -1,4 +1,5 @@
-import requests as req                                                                                                             
+import requests as req
+import threading
 import os,telegram,re,random,time,json
 mixtime = int(input('请输入两次下载最小间隔时间，单位为秒:'))
 maxtime = int(input('请输入两次下载最大间隔时间，单位为秒:'))
@@ -6,19 +7,6 @@ while(mixtime >= maxtime):
     print('最大间隔时间小于或等于最小间隔时间,请重新输入!')
     mixtime = int(input('请输入两次下载最小间隔时间，单位为秒:'))
     maxtime = int(input('请输入两次下载最大间隔时间，单位为秒:'))
-if os.path.exists('1.txt') == True:
-    txt = open('./1.txt', 'r')
-    string = txt.read()
-    txt.close()
-    sum_dict = json.loads(string)
-    sum = int(sum_dict['sum'])
-else:
-    sum_dict = {"sum": "0"}
-    string = json.dumps(sum_dict)
-    txt = open('./1.txt', 'w')
-    txt.write(string)
-    txt.close()
-    sum = 0
     
 
 ci = 0                                                                                                                             
@@ -30,7 +18,8 @@ headers = {
             'user-agent': r'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'                                                                                                                               
          }                                                                                                                         
 def downloadpng1(i):                                                                                                               
-    url = r'https://www.mzitu.com/'+str(i)                                                                                         
+    url = r'https://www.mzitu.com/'+str(i)
+    #print(url)
     h = req.get(url,cookies=cookies,headers=headers)                                                                               
     if h.status_code == 200:                                                                                                       
         p = re.search(r'pagenavi.*hotlist',h.text,re.DOTALL).group(0)                                                              
@@ -47,15 +36,34 @@ def downloadpng2(a,b):
             downlink = re.findall(r'<img src="(.*?)" alt="',m.text,re.DOTALL)                                                      
             return downlink[0]                                                                                                     
         else:                                                                                                                      
-            return 0                                                                                                               
-for x in range(sum+1,210000):                                                                                                        
-    print('正在下载id为'+str(x)+"的图片集")                                                                                                                       
-    b = downloadpng1(x)                                                                                                            
+            return 0
+def all():
+    if os.path.exists('1.txt') == True:
+        txt = open('./1.txt', 'r')
+        string = txt.read()
+        txt.close()
+        sum_dict = json.loads(string)
+        sum = int(sum_dict['sum'])
+    else:
+        sum_dict = {"sum": "1"}
+        string = json.dumps(sum_dict)
+        txt = open('./1.txt', 'w')
+        txt.write(string)
+        txt.close()
+        sum = 1                                                                                                           
+    print('正在下载id为'+str(sum)+"的图片集")                                                                                                                       
+    b = downloadpng1(sum)                                                                                                            
     #print(b)                                                                                                                      
-    if b != 0 :                                                                                                                    
+    if b != 0 :
+        sum +=1
+        sum_dict['sum'] = str(sum)
+        string = json.dumps(sum_dict)
+        txt = open('./1.txt', 'w')
+        txt.write(string)
+        txt.close()
         for v in range(1,b+1):                                                                                                     
             print('正在下载该图片集下第'+str(v)+'本')                                                                                                               
-            c = downloadpng2(x,v)                                                                                                  
+            c = downloadpng2(sum-1,v)                                                                                                  
             if c!=0:                                                                                                               
                 d = req.get(c,cookies=cookies,headers=headers)                                                                     
                 print('该图片集下第'+str(v)+'本状态响应码为:'+str(d.status_code))                                                                                               
@@ -63,21 +71,8 @@ for x in range(sum+1,210000):
                     f.write(d.content)                                                                                             
                 img = open('1.jpg', 'rb')                                                                                          
                 bot.send_photo(chat_id='@baisimeitu', photo=img)                                                                   
-                ci +=1
-                sum +=1
-                sum_dict['sum'] = str(sum)
-                string = json.dumps(sum_dict)
-                txt = open('./1.txt', 'w')
-                txt.write(string)
-                txt.close()
 
-                if ci%10==0:                                                                                                       
-                    time.sleep(180)                                                                                                
-                elif ci%90==0:                                                                                                     
-                    time.sleep(600)                                                                                                
-                else:                                                                                                              
-                                                                                                                                   
-                    time.sleep(random.randint(mixtime,maxtime))                                                                              
+        time.sleep(random.randint(mixtime,maxtime))                                                                              
                                                                                                                                    
     else:
         sum +=1
@@ -86,5 +81,16 @@ for x in range(sum+1,210000):
         txt = open('./1.txt', 'w')
         txt.write(string)
         txt.close()                   
-        time.sleep(5)                                                                                                              
-        continue
+        time.sleep(1)
+def main():
+    t1 = threading.Thread(target=all)
+    t2 = threading.Thread(target=all)
+    t1.start()
+    t2.start()
+
+    # 等待两个子线程结束再结束主线程
+    t1.join()
+    t2.join()
+
+if __name__ == '__main__':
+    main()
