477fbf3e79ffd4211e41648b4ec48230aed1a342
mikulo <46894621+mikulo@users.noreply.github.com>
2019-11-30 15:35:23 +0800

    Update gdlink.php

--- gdlink.php
+++ gdlink.php
@@ -2,7 +2,6 @@
 <body>
 
 <?php 
-//谷歌直链大文件方法代码来源为:https://github.com/donwa/GoogleDriveDirect
 //定义ip国家子函数,如果从中国访问,返回1，否则返回0
 function get_ip_lookup($ip=null){
     $res = @file_get_contents('http://ip.taobao.com/service/getIpInfo.php?ip=' . $ip);
@@ -16,13 +15,24 @@ function get_ip_lookup($ip=null){
     }
     
 }
+//定义cf返回code函数
+function getcode($url){
+    $ch = curl_init($url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+    curl_exec($ch);
+    $httpcode = curl_getinfo($ch,CURLINFO_HTTP_CODE); 
+    curl_close($ch);
+    return $httpcode;
+}
+
 //添加cf直链网址到数组
-$address = array("https://link.leng.workers.dev/link/",
-				 "https://link.leng1.workers.dev/link/",
-				 "https://link.leng2.workers.dev/link/",
-				 "https://link.leng3.workers.dev/link/"
+$address = array("https://link.leng.workers.dev",
+				 "https://link.leng1.workers.dev",
+				 "https://link.leng2.workers.dev",
+				 "https://link.leng3.workers.dev"
 			
 );
+
 $id = $_GET["id"];
 $mode = $_GET["mode"];
 $ip = $_SERVER["REMOTE_ADDR"];
@@ -49,10 +59,22 @@ elseif($mode=="0")
 {	
 	//获取随机站点
 	$num = rand(0,count($address)-1);
-	$link = "$address[$num]$id";
-	//echo $num+1;
-	header("Location: $link");
-	exit;
+	while (true)
+	{	//判断cf站点是否可用，不可用继续随机挑选到可用为止
+		if (getcode($address[$num])== 200)
+		{
+			$link = "$address[$num]/link/$id";
+			//echo $num+1;
+			header("Location: $link");
+			exit;	
+		}
+		else
+		{
+			$num = rand(0,count($address)-1);
+		}
+	}
+
+
 }
 elseif($mode=="1")
 {	
