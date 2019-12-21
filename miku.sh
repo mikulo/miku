0fc0ccbdca812a2cdd35e10e21b24c415fa9a44f
mikulo <46894621+mikulo@users.noreply.github.com>
2019-12-21 14:34:21 +0800

    Update miku.sh

--- miku.sh
+++ miku.sh
@@ -31,7 +31,7 @@ fi
 #fi
 echo -e "\033[32m欢迎使用多功能脚本,请输入序号选择功能\033[0m"
 echo -e "\033[32m脚本仅支持debian/ubuntu系统!\033[0m" 
-echo -e "\033[32m当前版本为:0.58\033[0m" 
+echo -e "\033[32m当前版本为:0.59\033[0m" 
 echo 1.科学上网脚本集合
 echo 2.各种工具脚本集合
 echo 3.系统优化脚本集合
@@ -201,9 +201,9 @@ then
 elif	[ "$m1" == 5 ]
 then
 	#wget -N --no-check-certificate "https://raw.githubusercontent.com/mikulo/miku/master/miku.sh" && chmod +x miku.sh
-	echo -e "\033[32m请执行wget -N --no-check-certificate "https://raw.githubusercontent.com/mikulo/miku/master/miku.sh" && chmod +x miku.sh&& ./miku.sh\n来手动更新脚本\033[0m"
-
-	
+	echo -e "\033[32m请手动执行:\033[0m"
+        echo -e "\033[32mwget -N --no-check-certificate "https://raw.githubusercontent.com/mikulo/miku/master/miku.sh" && chmod +x miku.sh&& ./miku.sh\033[0m"
+	echo -e "\033[32m来更新脚本\033[0m"
 elif	[ "$m1" == 0 ]
 then
 	
