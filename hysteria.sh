1fea5da6999d5229ad85886b605d4da377c7d7e5
mikulo <46894621+mikulo@users.noreply.github.com>
2022-01-18 19:48:48 +0800

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -6,6 +6,37 @@ else
 	echo -e "\033[32m请用root用户执行本脚本!\033[0m"
         exit
 fi
+
+option1(){
+echo "请输入端口:"
+read -p ">>" port
+echo "请输入域名(确保已解析到本机IP):"
+read -p ">>" domain
+echo "请输入申请ssl的邮箱:"
+read -p ">>" email
+echo "请输入obfs:"
+read -p ">>" obfs
+echo "请输入上传限速,单位为m:"
+read -p ">>" up_mbps
+echo "请输入下载限速,单位为m:"
+read -p ">>" down_mbps
+cat>config.json<<EOF
+{
+  "listen": ":${port}",
+  "acme": {
+    "domains": [
+      "${domain}"
+    ],
+    "email": "${email}"
+  },
+  "obfs": "${obfs}",
+  "up_mbps": ${up_mbps},
+  "down_mbps": ${down_mbps}
+}
+EOF
+
+
+}
 echo -e "\033[32m欢迎使用hysteria一键安装脚本\033[0m"
 echo -e "\033[32m脚本仅支持debian/ubuntu系统!\033[0m" 
 echo -e "\033[32m请选择系统架构\033[0m" 
@@ -20,24 +51,29 @@ then
 	wget https://github.com/HyNetwork/hysteria/releases/download/v0.9.4/hysteria-tun-linux-amd64
   chmod 777 hysteria-tun-linux-amd64
   mv hysteria-tun-linux-amd64 hysteria
+  option1
+  ./hysteria server
 elif	[ "$m1" == 2 ]
 then
 	wget https://github.com/HyNetwork/hysteria/releases/download/v0.9.4/hysteria-tun-linux-arm64
   chmod 777 hysteria-tun-linux-arm64
   mv hysteria-tun-linux-arm64 hysteria
-	
+  option1
+  ./hysteria server		
 elif	[ "$m1" == 3 ]
 then
 	wget https://github.com/HyNetwork/hysteria/releases/download/v0.9.4/hysteria-tun-linux-arm-7
   chmod 777 hysteria-tun-linux-arm-7
   mv hysteria-tun-linux-arm-7 hysteria
-  
+  option1
+  ./hysteria server
 elif	[ "$m1" == 4 ]
 then
-	wget https://github.com/HyNetwork/hysteria/releases/download/v0.9.4/hysteria-tun-linux-386
+  wget https://github.com/HyNetwork/hysteria/releases/download/v0.9.4/hysteria-tun-linux-386
   chmod 777 hysteria-tun-linux-386 
   mv hysteria-tun-linux-386 hysteria
-
+  option1
+  ./hysteria server
 elif	[ "$m1" == 0 ]
 then
 	echo -e "\033[32m退出完成,执行./hysteria.sh再次打开本脚本\033[0m"	
